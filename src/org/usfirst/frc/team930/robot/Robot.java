8eb45e054d136b1898ba2a375d86c72a2d30f677
Owen Zinkgraf <ozinkgraf@gmail.com>
2015-01-28 19:21:41 -0600

    First commit

---
+++ src/org/usfirst/frc/team930/robot/Robot.java
@@ -0,0 +1,60 @@
+//HMS 930
+
+package org.usfirst.frc.team930.robot;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.Scheduler;
+import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+
+import org.usfirst.frc.team930.robot.commands.Drive;
+import org.usfirst.frc.team930.robot.commands.ExampleCommand;
+import org.usfirst.frc.team930.robot.subsystems.Arm;
+import org.usfirst.frc.team930.robot.subsystems.Claw;
+import org.usfirst.frc.team930.robot.subsystems.Drivetrain;
+import org.usfirst.frc.team930.robot.subsystems.ExampleSubsystem;
+
+
+public class Robot extends IterativeRobot {
+
+	public static final Arm arm = new Arm();
+	public static final Claw claw = new Claw();
+	public static final Drivetrain drivetrain = new Drivetrain();
+	public static OI oi;
+
+    Command drive;
+
+ 
+    public void robotInit() {
+		oi = new OI();
+        drive = new Drive();
+    }
+	
+	public void disabledPeriodic() {
+		Scheduler.getInstance().run();
+	}
+
+    public void autonomousInit() {
+        if (autonomousCommand != null) autonomousCommand.start();
+    }
+
+    public void autonomousPeriodic() {
+        Scheduler.getInstance().run();
+    }
+
+    public void teleopInit() {
+        if (autonomousCommand != null) autonomousCommand.cancel();
+    }
+
+    public void disabledInit(){
+
+    }
+
+    public void teleopPeriodic() {
+        Scheduler.getInstance().run();
+    }
+    
+    public void testPeriodic() {
+        LiveWindow.run();
+    }
+}
