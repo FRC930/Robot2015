997d4983ef1cfd671465e89f27cbcbda3643a851
owengoedland17 <owengoedland17@easttroy.k12.wi.us>
2015-01-31 16:50:20 -0600

    Updates the arm set height

--- src/org/usfirst/frc/team930/robot/commands/Drive.java
+++ src/org/usfirst/frc/team930/robot/commands/Drive.java
@@ -10,7 +10,7 @@ public class Drive extends Command {
     public Drive() {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
-    }
+     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
