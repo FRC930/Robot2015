d5c242d0b7813002ac24ff42562d28702cc35c59
Jensen215 <alex@jensen-web.com>
2015-02-09 17:54:18 -0600

    added relays to the claw code

--- src/org/usfirst/frc/team930/robot/Robot.java
+++ src/org/usfirst/frc/team930/robot/Robot.java
@@ -8,12 +8,9 @@ import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 
 import org.usfirst.frc.team930.robot.commands.Drive;
-import org.usfirst.frc.team930.robot.commands.ExampleCommand;
 import org.usfirst.frc.team930.robot.subsystems.Arm;
 import org.usfirst.frc.team930.robot.subsystems.Claw;
 import org.usfirst.frc.team930.robot.subsystems.Drivetrain;
-import org.usfirst.frc.team930.robot.subsystems.ExampleSubsystem;
-
 
 public class Robot extends IterativeRobot {
 
@@ -22,39 +19,42 @@ public class Robot extends IterativeRobot {
 	public static final Drivetrain drivetrain = new Drivetrain();
 	public static OI oi;
 
-    Command drive;
+	Command drive;
+	Command closedclaw;
+	Command openclaw;
 
- 
-    public void robotInit() {
+	public void robotInit() {
 		oi = new OI();
-        drive = new Drive();
-    }
-	
+		drive = new Drive();
+	}
+
 	public void disabledPeriodic() {
 		Scheduler.getInstance().run();
 	}
 
-    public void autonomousInit() {
-        if (autonomousCommand != null) autonomousCommand.start();
-    }
+	public void autonomousInit() {
+	//	if (autonomousCommand != null)
+			//autonomousCommand.start();
+	}
 
-    public void autonomousPeriodic() {
-        Scheduler.getInstance().run();
-    }
+	public void autonomousPeriodic() {
+		Scheduler.getInstance().run();
+	}
 
-    public void teleopInit() {
-        if (autonomousCommand != null) autonomousCommand.cancel();
-    }
+	public void teleopInit() {
+		//if (autonomousCommand != null)
+			//autonomousCommand.cancel();
+	}
 
-    public void disabledInit(){
+	public void disabledInit() {
 
-    }
+	}
 
-    public void teleopPeriodic() {
-        Scheduler.getInstance().run();
-    }
-    
-    public void testPeriodic() {
-        LiveWindow.run();
-    }
+	public void teleopPeriodic() {
+		Scheduler.getInstance().run();
+	}
+
+	public void testPeriodic() {
+		LiveWindow.run();
+	}
 }
--- src/org/usfirst/frc/team930/robot/subsystems/Claw.java
+++ src/org/usfirst/frc/team930/robot/subsystems/Claw.java
@@ -1,18 +1,57 @@
 package org.usfirst.frc.team930.robot.subsystems;
 
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Relay;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
  *
  */
 public class Claw extends Subsystem {
-    
-    // Put methods for controlling this subsystem
-    // here. Call these from Commands.
 
-    public void initDefaultCommand() {
-        // Set the default command for a subsystem here.
-        //setDefaultCommand(new MySpecialCommand());
-    }
+	// Put methods for controlling this subsystem
+	// here. Call these from Commands.
+	Relay relay2;
+	Relay relay1;
+	DigitalInput limitSwitch1;
+	DigitalInput limitSwitch2 ;
+	
+	public Claw(){ // Constructor
+		relay1 = new Relay(1);					
+		relay2 = new Relay(2);
+		limitSwitch1 = new DigitalInput(1);
+		limitSwitch2 = new DigitalInput(2);
+	}
+	
+	public void initDefaultCommand() {
+		
+		//Sometimes the world is a better place with a little more onions 
+		// setDirection the default command for a subsystem here.
+		// setDirectionDefaultCommand(new MySpecialCommand());
+	}
+
+	public void openClaw() {
+		if (limitSwitch2.get() == false) {		//Control for open claw
+			relay1.set(Relay.Value.kReverse);
+			relay2.set(Relay.Value.kReverse);
+
+		} else {
+			relay1.set(Relay.Value.kOff);						//Control for close claw, opposite
+			relay2.set(Relay.Value.kOff);		
+		}
+
+	}
+
+	public void closeClaw() {
+		if (limitSwitch1.get() == false) {	//Control for close claw
+			relay1.set(Relay.Value.kForward);
+			relay2.set(Relay.Value.kForward);
+
+		} else {
+			relay1.set(Relay.Value.kOff);					//Control for open claw, opposite
+			relay2.set(Relay.Value.kOff);	
+
+		}
+	}
 }
-
