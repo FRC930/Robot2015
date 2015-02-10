1a3a296f246e8a1def50135e790dae1520b96773
owengoedland17 <owengoedland17@easttroy.k12.wi.us>
2015-02-10 17:47:42 -0600

    Updated arm code

--- src/org/usfirst/frc/team930/robot/OI.java
+++ src/org/usfirst/frc/team930/robot/OI.java
@@ -1,13 +1,13 @@
 package org.usfirst.frc.team930.robot;
 
-<<<<<<< HEAD
+
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.buttons.Button;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
 
 import org.usfirst.frc.team930.robot.commands.ArmHeight0;
 import org.usfirst.frc.team930.robot.commands.ArmHeight1;
-import org.usfirst.frc.team930.robot.commands.ExampleCommand;
+
 import org.usfirst.frc.team930.robot.commands.MoveArmDown;
 import org.usfirst.frc.team930.robot.commands.MoveArmUp;
 //XXX_666taintedGangaGod_XXX
@@ -15,8 +15,7 @@ import org.usfirst.frc.team930.robot.commands.MoveArmUp;
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
  */
-=======
->>>>>>> 4cfc9b9d6c139a751576b71321a3eccd7495e7e7
+
 public class OI {
 	
 	public static OI getInstance(){
