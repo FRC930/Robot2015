c92b1327385051e61189d324393a8143c4632999
owengoedland17 <owengoedland17@easttroy.k12.wi.us>
2015-02-10 18:40:06 -0600

    Updateded Arm Code

--- src/org/usfirst/frc/team930/robot/OI.java
+++ src/org/usfirst/frc/team930/robot/OI.java
@@ -7,9 +7,9 @@ import edu.wpi.first.wpilibj.buttons.JoystickButton;
 
 import org.usfirst.frc.team930.robot.commands.ArmHeight0;
 import org.usfirst.frc.team930.robot.commands.ArmHeight1;
-
 import org.usfirst.frc.team930.robot.commands.MoveArmDown;
 import org.usfirst.frc.team930.robot.commands.MoveArmUp;
+
 //XXX_666taintedGangaGod_XXX
 /**
  * This class is the glue that binds the controls on the physical operator
@@ -21,11 +21,11 @@ public class OI {
 	public static OI getInstance(){
 		return Holder.instance;
 	}
-	private OI(){
-		incUp.whenPressed(new ArmHeight1());
-		incDown.whenPressed(new ArmHeight0());
-		goUp.whileHeld(new MoveArmUp());
-		goDown.whileHeld(new MoveArmDown()); 
+	public OI(){
+//		incUp.whenPressed(new ArmHeight1());
+//		incDown.whenPressed(new ArmHeight0());
+//		goUp.whileHeld(new MoveArmUp());
+//		goDown.whileHeld(new MoveArmDown()); 
 
 // XXX_Druk3nRainbowPig_XXX
 	}
@@ -34,16 +34,21 @@ public class OI {
 		public static final OI instance = new OI();
 	}
 	Joystick stick2 = new Joystick(0);
-	Button incUp = new JoystickButton(stick2, 3);
-	Button incDown = new JoystickButton(stick2, 0);
-	Button goUp = new JoystickButton(stick2, 1);
-	Button goDown = new JoystickButton(stick2, 2);
+//	Button incUp = new JoystickButton(stick2, 3);
+//	Button incDown = new JoystickButton(stick2, 0);
+//	Button goUp = new JoystickButton(stick2, 1);
+//	Button goDown = new JoystickButton(stick2, 2);
 	
 	
 	// XXX_P0tat0*.C0mM_XXX
+	public double getAxisY(){
+	return 0.5*stick2.getRawAxis(1);
 	
+	}
+
 }
 	
+
     //// CREATING BUTTONS
     // One type of button is a joystick button which is any button on a joystick.
     // You create one by telling it which joystick it's on and which button
