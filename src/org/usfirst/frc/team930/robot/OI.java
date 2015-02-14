963c6d43c40912e93d054a014d0e9828ecd54c65
owengoedland17 <owengoedland17@easttroy.k12.wi.us>
2015-02-14 17:01:54 -0600

    Buttons with claw

--- src/org/usfirst/frc/team930/robot/OI.java
+++ src/org/usfirst/frc/team930/robot/OI.java
@@ -2,8 +2,16 @@ package org.usfirst.frc.team930.robot;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
 
 public class OI {
-	Joystick stick = new Joystick(1);
+	Joystick driver = new Joystick(1);
+	Joystick codriver = new Joystick(2); 
+	
+	JoystickButton aButton = new JoystickButton(driver, 4);
+	
+	public OI (){
+		aButton.whenPressed(closeClaw);
+	}
 }
 
--- src/org/usfirst/frc/team930/robot/subsystems/Grip.java
+++ src/org/usfirst/frc/team930/robot/subsystems/Grip.java
@@ -1,40 +1,41 @@
 package org.usfirst.frc.team930.robot.subsystems;
-
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Relay;
 
 public class Grip {
+	public static Relay leftRelay = new Relay(0);
+	public static Relay rightRelay = new Relay(1);
+	public static DigitalInput rightOpen = new DigitalInput(0);
+	public static DigitalInput leftOpen = new DigitalInput(1);
+	public static DigitalInput rightClosed = new DigitalInput(2);
+	public static DigitalInput leftClosed = new DigitalInput(3);
+	
 	Relay r;
-	DigitalInput limitSwitch1;
-	DigitalInput limitSwitch2;
+	DigitalInput limitSwitchOpen;
+	DigitalInput limitSwitchClosed;
+	
+	public Grip() {
 
-
-public Grip(){
-	
-	
-	
-}
-
-public Grip(Relay input, DigitalInput l1, DigitalInput l2){
-	r = input;
-	limitSwitch1 = l1;
-	limitSwitch2 = l2;
-	
-	
-}
-public void openClaw() {
-	while (limitSwitch1.get() != true){
-		r.set(Relay.Value.kReverse);
 	}
-}
 
-public void closeClaw() {
-	while (limitSwitch2.get() != true){
-		r.set(Relay.Value.kForward);
+	public Grip(Relay input, DigitalInput l1, DigitalInput l2) {
+		r = input;
+		limitSwitchOpen = l1;
+		limitSwitchClosed = l2;
+
 	}
-}
 
+	public void openClaw() {
+		while (limitSwitchOpen.get() == true) {
+			r.set(Relay.Value.kReverse);
+		}
+		r.set(Relay.Value.kOff);
+	}
 
-
-
+	public void closeClaw() {
+		while (limitSwitchClosed.get() == true) {
+			r.set(Relay.Value.kForward);
+		}
+		r.set(Relay.Value.kOff);
+	}
 }
