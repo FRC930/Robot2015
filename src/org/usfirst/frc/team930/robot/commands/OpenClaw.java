7568708443ed7bc87bbaad6277e8fce8de569b06
Owen Zinkgraf <ozinkgraf@gmail.com>
2015-02-14 20:31:21 -0600

    changed squentialy to parellely

--- src/org/usfirst/frc/team930/robot/commands/OpenClaw.java
+++ src/org/usfirst/frc/team930/robot/commands/OpenClaw.java
@@ -24,5 +24,8 @@ public class OpenClaw extends CommandGroup {
         // e.g. if Command1 requires chassis, and Command2 requires arm,
         // a CommandGroup containing them would require both the chassis and the
         // arm.
+    	
+    	addParallel(new OpenLeftClaw());
+    	addParallel(new OpenRightClaw());
     }
 }
