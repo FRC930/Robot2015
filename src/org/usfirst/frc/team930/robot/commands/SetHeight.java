b94107f01c11863d3bfb008605893d3f314ea493
Nick Janovetz <cubbix1@gmail.com>
2015-02-14 22:13:04 -0600

    Valentines Night, and the programmers have no dates. This is why we code... not to make a robot, but to see if we can find dates in our code. Its late, last commit here before Sussex. l8r

--- src/org/usfirst/frc/team930/robot/commands/SetHeight.java
+++ src/org/usfirst/frc/team930/robot/commands/SetHeight.java
@@ -4,42 +4,55 @@ import org.usfirst.frc.team930.robot.OI;
 import org.usfirst.frc.team930.robot.Robot;
 
 import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class SetHeight extends Command {
 
-	final double HEIGHT_MAX = 46;
-	final double HEIGHT_MIN = 44;
+	final double HEIGHT_MAX = 65;
+	final double HEIGHT_MIN = 10;
 	final double HEIGHT_OF_ROBOT = 45.25;
 	final double LENGTH_OF_ARM = 47.0;
+
+	double angle;
 
 	OI oi = OI.getInstance();
 
 	public SetHeight() {
 		requires(Robot.arm);
+		// SmartDashboard.putBoolean("Update", false);
+		// SmartDashboard.putNumber("ArmHeight (inches)", 40);
 	}
 
 	// Called just before this Command runs the first time
 	protected void initialize() {
+		SmartDashboard.putBoolean("Update", false);
+		SmartDashboard.putNumber("ArmHeight (inches)", 40);
 	}
 
 	// Called repeatedly when this Command is scheduled to run
 	protected void execute() {
 		// height in inches from the floor
-		double height = HEIGHT_MIN + (HEIGHT_MAX - HEIGHT_MIN) * oi.getArmHeight();
+		if (SmartDashboard.getBoolean("Update")) {
+			double height = SmartDashboard.getNumber("ArmHeight (inches)");
+			/* HEIGHT_MIN + (HEIGHT_MAX - HEIGHT_MIN) * oi.getArmHeight(); */
 
-		if (height > HEIGHT_MAX)
-			height = HEIGHT_MAX;
-		else if (height < HEIGHT_MIN)
-			height = HEIGHT_MIN;
-		
-		double angle = Math.asin((height - HEIGHT_OF_ROBOT) / LENGTH_OF_ARM);
+			if (height > HEIGHT_MAX)
+				height = HEIGHT_MAX;
+			else if (height < HEIGHT_MIN)
+				height = HEIGHT_MIN;
 
-		Robot.arm.setAngle(angle);
+			angle = Math.asin((height - HEIGHT_OF_ROBOT) / LENGTH_OF_ARM);
+
+			Robot.arm.setAngle(angle);
+		}
 	}
 
 	// Make this return true when this Command no longer needs to run execute()
 	protected boolean isFinished() {
-		return false;
+		if (Robot.arm.getArmAngle() == angle)
+			return true;
+		else
+			return false;
 	}
 
 	// Called once after isFinished returns true
